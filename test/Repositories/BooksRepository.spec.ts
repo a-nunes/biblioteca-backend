cf6892321a3870b2895ec50c9aaa18e814017574
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-20 19:10:16 -0300

    refactor: isolate sut creation

--- test/Repositories/BooksRepository.spec.ts
+++ test/Repositories/BooksRepository.spec.ts
@@ -1,28 +1,22 @@
+import { BookDTO } from 'App/Dto/BookDTO'
+import { Book } from 'App/Models/Book'
 import BooksRepository from 'App/Repositories/BooksRepository'
 import test from 'japa'
 
-test.group('BooksRepository', () => {
-  test.group('delete', () => {
-    test('assert delete returns undefined if book is not found', (assert) => {
-      const sut = new BooksRepository()
-      const result = sut.delete(1)
-      assert.isUndefined(result)
-    })
+test('BooksRepository', () => {
+  test.group('delete', (group) => {
+    let booksAfterDeletion: Book[]
+    let book: BookDTO
+    let sut: BooksRepository
 
-    test('assert delete remove book if its found', (assert) => {
-      const sut = new BooksRepository()
-      const book = {
+    group.before(() => {
+      book = {
         title: 'Harry Potter 4',
         publisher: 'Rocco',
         image: 'https://i.imgur.com/UH3IPXw.jpg',
         authors: ['J. K. Rowling', '...'],
       }
-      sut.add(book)
-      sut.add(book)
-      sut.delete(1)
-      const books = sut.list()
-
-      assert.deepEqual(books, [
+      booksAfterDeletion = [
         {
           id: 2,
           titulo: 'Harry Potter 4',
@@ -30,7 +24,25 @@ test.group('BooksRepository', () => {
           foto: 'https://i.imgur.com/UH3IPXw.jpg',
           autores: ['J. K. Rowling', '...'],
         },
-      ])
+      ]
+    })
+
+    group.beforeEach(() => {
+      sut = new BooksRepository()
+    })
+
+    test('assert delete returns undefined if book is not found', (assert) => {
+      const result = sut.delete(1)
+      assert.isUndefined(result)
+    })
+
+    test('assert delete remove book if its found', (assert) => {
+      sut.add(book)
+      sut.add(book)
+      sut.delete(1)
+      const books = sut.list()
+
+      assert.deepEqual(books, booksAfterDeletion)
     })
   })
 })
