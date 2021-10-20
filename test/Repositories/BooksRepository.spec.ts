8293ccdb882d6af43ed1e1a04ae1adbcc571e21a
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-20 18:42:39 -0300

    test: ensure delete remove book if its found

--- test/Repositories/BooksRepository.spec.ts
+++ test/Repositories/BooksRepository.spec.ts
@@ -8,5 +8,29 @@ test.group('BooksRepository', () => {
       const result = sut.delete(1)
       assert.isUndefined(result)
     })
+
+    test('assert delete remove book if its found', (assert) => {
+      const sut = new BooksRepository()
+      const book = {
+        title: 'Harry Potter 4',
+        publisher: 'Rocco',
+        image: 'https://i.imgur.com/UH3IPXw.jpg',
+        authors: ['J. K. Rowling', '...'],
+      }
+      sut.add(book)
+      sut.add(book)
+      sut.delete(1)
+      const books = sut.list()
+
+      assert.deepEqual(books, [
+        {
+          id: 2,
+          titulo: 'Harry Potter 4',
+          editora: 'Rocco',
+          foto: 'https://i.imgur.com/UH3IPXw.jpg',
+          autores: ['J. K. Rowling', '...'],
+        },
+      ])
+    })
   })
 })
