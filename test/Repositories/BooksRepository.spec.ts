0072299c7a33a41e3023b59cdc0b97351f50f8b5
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-20 20:32:28 -0300

    refactor: improve code standard and quality

--- test/Repositories/BooksRepository.spec.ts
+++ test/Repositories/BooksRepository.spec.ts
@@ -7,8 +7,9 @@ test('BooksRepository', () => {
   let book: BookDTO
   let sut: BooksRepository
 
-  test.group('delete', (group) => {
-    let booksAfterDeletion: Book[]
+  test.group('update', (group) => {
+    let newBook: BookDTO
+    let updatedBook: Book
 
     group.before(() => {
       book = {
@@ -17,6 +18,41 @@ test('BooksRepository', () => {
         image: 'https://i.imgur.com/UH3IPXw.jpg',
         authors: ['J. K. Rowling', '...'],
       }
+      newBook = {
+        title: 'Harry Potter 5',
+        publisher: 'Rocco 1',
+        image: 'https://i.imgur.com/UH3IPXw.jpg',
+        authors: ['J. K. Rowling', '...'],
+      }
+      updatedBook = {
+        id: 1,
+        titulo: 'Harry Potter 5',
+        editora: 'Rocco 1',
+        foto: 'https://i.imgur.com/UH3IPXw.jpg',
+        autores: ['J. K. Rowling', '...'],
+      }
+    })
+
+    group.beforeEach(() => {
+      sut = new BooksRepository()
+    })
+
+    test('assert update returns undefined if book is not found', (assert) => {
+      const result = sut.update(1, book)
+      assert.isUndefined(result)
+    })
+
+    test('assert update returns updated book if its found', (assert) => {
+      sut.add(book)
+      const result = sut.update(1, newBook)
+      assert.deepEqual(result, updatedBook)
+    })
+  })
+
+  test.group('delete', (group) => {
+    let booksAfterDeletion: Book[]
+
+    group.before(() => {
       booksAfterDeletion = [
         {
           id: 2,
@@ -28,10 +64,6 @@ test('BooksRepository', () => {
       ]
     })
 
-    group.beforeEach(() => {
-      sut = new BooksRepository()
-    })
-
     test('assert delete returns undefined if book is not found', (assert) => {
       const result = sut.delete(1)
       assert.isUndefined(result)
@@ -39,42 +71,10 @@ test('BooksRepository', () => {
 
     test('assert delete remove book if its found', (assert) => {
       sut.add(book)
-      sut.add(book)
       sut.delete(1)
       const books = sut.list()
 
       assert.deepEqual(books, booksAfterDeletion)
-    })
-  })
-
-  test.group('update', (group) => {
-    let newBook: BookDTO
-    let updatedBook: Book
-
-    group.before(() => {
-      newBook = {
-        title: 'Harry Potter 5',
-        publisher: 'Rocco 1',
-        image: 'https://i.imgur.com/UH3IPXw.jpg',
-        authors: ['J. K. Rowling', '...'],
-      }
-      updatedBook = {
-        id: 2,
-        titulo: 'Harry Potter 5',
-        editora: 'Rocco 1',
-        foto: 'https://i.imgur.com/UH3IPXw.jpg',
-        autores: ['J. K. Rowling', '...'],
-      }
-    })
-
-    test('assert update returns updated book if its found', (assert) => {
-      const result = sut.update(2, newBook)
-      assert.deepEqual(result, updatedBook)
-    })
-
-    test('assert update returns undefined if book is not found', (assert) => {
-      const result = sut.update(1, book)
-      assert.isUndefined(result)
     })
   })
 })
