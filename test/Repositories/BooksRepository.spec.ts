14f6380e097bccc35f4248580500d293ca3b2e88
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-24 09:07:49 -0300

    test: ensure delete remove book if its found

--- test/Repositories/BooksRepository.spec.ts
+++ test/Repositories/BooksRepository.spec.ts
@@ -18,13 +18,16 @@ test.group('add', (group) => {
     const params = bookFactory.build()
     const book = booksRepository.add(params)
     const books = booksRepository.list()
+
     assert.deepEqual(books[books.length - 1], book)
   })
 
   test('assert books length is increment by 1', (assert) => {
     const params = bookFactory.build()
     booksRepository.add(params)
+
     const books = booksRepository.list()
+
     assert.deepEqual(books.length, 1)
   })
 })
@@ -45,7 +48,9 @@ test.group('list', (group) => {
     const book1 = booksRepository.add(params1)
     const params2 = bookFactory.build({ title: 'Harry Potter 2' })
     const book2 = booksRepository.add(params2)
+
     const books = booksRepository.list()
+
     assert.deepEqual(books, [book1, book2])
   })
 })
@@ -64,19 +69,24 @@ test.group('delete', (group) => {
   test('assert delete returns undefined if book is not found', (assert) => {
     const params = bookFactory.build()
     const book = booksRepository.add(params)
+
     const result = booksRepository.delete(2)
     const books = booksRepository.list()
+
     assert.isUndefined(result)
     assert.include(books, book)
   })
 
-  // test('assert delete remove book if its found', (assert) => {
-  //   booksRepository.add(book)
-  //   booksRepository.delete(1)
-  //   const books = booksRepository.list()
+  test('assert delete remove book if its found', (assert) => {
+    const params = bookFactory.build()
+    const book = booksRepository.add(params)
 
-  //   assert.deepEqual(books, booksAfterDeletion)
-  // })
+    booksRepository.delete(1)
+    const books = booksRepository.list()
+
+    assert.notInclude(books, book)
+    assert.isEmpty(books)
+  })
 })
 
 // test.group('list', () => {
