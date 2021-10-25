bcd91d9fc618d8943163cce408717a4c55f80919
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-25 17:02:57 -0300

    test: ensure it returns 201 and id on POST success

--- test/Controllers/BooksController.spec.ts
+++ test/Controllers/BooksController.spec.ts
@@ -5,8 +5,15 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group('index', () => {
+test.group('GET /obras', () => {
   test('ensure returns 200 on success', async () => {
     await supertest(BASE_URL).get('/obras').expect(200)
+  })
+})
+
+test.group('POST /obras', () => {
+  test('ensure returns 201 and book on success', async () => {
+    const params = bookFactory.build()
+    await supertest(BASE_URL).post('/obras').set(params).expect(201, { id: 1 })
   })
 })
