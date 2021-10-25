7108fc3b9f1b626c24d6231cdeede2f3e4bf76f9
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-25 17:47:23 -0300

    test: ensure it returns 201 and id on POST success

--- test/Controllers/BooksController.spec.ts
+++ test/Controllers/BooksController.spec.ts
@@ -2,6 +2,8 @@ import bookFactory from '../Factories/Book'
 
 import test from 'japa'
 import supertest from 'supertest'
+import faker from 'faker'
+import BookParser from 'App/Parsers/BookParser'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -13,7 +15,16 @@ test.group('GET /obras', () => {
 
 test.group('POST /obras', () => {
   test('ensure returns 201 and book on success', async () => {
-    const params = bookFactory.build()
-    await supertest(BASE_URL).post('/obras').set(params).expect(201, { id: 1 })
+    const params = {
+      title: faker.lorem.words(2),
+      publisher: faker.company.companyName(),
+      image: faker.internet.url(),
+      authors: [faker.name.findName()],
+    }
+    const parsedParams = new BookParser(params).parse()
+    await supertest(BASE_URL)
+      .post('/obras')
+      .send(params)
+      .expect(201, { ...parsedParams, id: 1 })
   })
 })
