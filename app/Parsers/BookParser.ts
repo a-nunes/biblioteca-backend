021d1a356d9f78a019a9980121ecc56ff956b6f7
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-26 13:31:47 -0300

    refactor: remove constructor and set input to method

--- app/Parsers/BookParser.ts
+++ app/Parsers/BookParser.ts
@@ -2,14 +2,12 @@ import { BookDTO } from 'App/Dto/BookDTO'
 import { BooksRepository } from 'App/Repositories/BooksRepository'
 
 export default class BookParser {
-  constructor(private readonly params: BookDTO) {}
-
-  public parse(): BooksRepository.Params {
+  public parse({ title, authors, image, publisher }: BookDTO): BooksRepository.Params {
     return {
-      titulo: this.params.title,
-      editora: this.params.publisher,
-      foto: this.params.image,
-      autores: this.params.authors,
+      titulo: title,
+      editora: publisher,
+      foto: image,
+      autores: authors,
     }
   }
 }
